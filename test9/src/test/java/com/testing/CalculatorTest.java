9df458d848af2ce41771b30e8218b6fe672aa06e
jaanus <zaar2213@gmail.com>
2024-12-15 00:38:26 +0200

    few more tests

--- test9/src/test/java/com/testing/CalculatorTest.java
+++ test9/src/test/java/com/testing/CalculatorTest.java
@@ -5,18 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    private final Calculator calc = new Calculator();
 
     @Test
     public void addingZeroPlusZero() {
-        Calculator calc = new Calculator();
         int num = calc.add(0, 0);
         assertEquals(0, num, "Expecting 0 + 0 to be 0");
     }
 
     @Test
     public void addingOnePlusOne() {
-        Calculator calc = new Calculator();
         int num = calc.add(1, 1);
         assertEquals(2, num, "Expecting 1 + 1 to be 2");
+    }
+
+    @Test
+    public void addingOnePlusMinusOne() {
+        int num = calc.add(1, -1);
+        assertEquals(0, num, "Expecting 1 + 1 to be 0");
+    }
+
+    @Test
+    public void addingMinusOnePlusMinusOne() {
+        int num = calc.add(-1, -1);
+        assertEquals(-2, num, "Expecting 1 + 1 to be 0");
     }
 }
