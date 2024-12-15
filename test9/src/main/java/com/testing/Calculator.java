2b3421429b5d3f8a3e3c1943e4957e9b78ae330b
jaanus <zaar2213@gmail.com>
2024-12-15 16:14:18 +0200

    testing annuity  calculation tests

--- test9/src/main/java/com/testing/Calculator.java
+++ test9/src/main/java/com/testing/Calculator.java
@@ -1,9 +1,36 @@
 package com.testing;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Calculator {
 
     public int add(int i, int j) {
         return i + j;
+    }
+
+    public int subtract(int i, int j) {
+        return i - j;
+    }
+
+    public int multiply(int i, int j) {
+        return i * j;
+    }
+
+    public int divide(int i, int j) {
+        return i / j;
+    }
+
+    public String calcAnnuity(String R, int t, String r, int n) {
+        BigDecimal a = new BigDecimal(r).divide(new BigDecimal(n));
+        BigDecimal b = BigDecimal.ONE.add(a);
+        BigDecimal c = b.pow(n * t);
+        BigDecimal d = c.subtract(BigDecimal.ONE);
+        BigDecimal e = d.divide(a);
+        BigDecimal f = e.multiply(new BigDecimal(R));
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance(Locale.US);
+        return currencyInstance.format(f);
     }
 
 }
--- test9/src/test/java/com/testing/CalculatorTest.java
+++ test9/src/test/java/com/testing/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.testing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,14 +22,55 @@ public class CalculatorTest {
     }
 
     @Test
-    public void addingOnePlusMinusOne() {
-        int num = calc.add(1, -1);
-        assertEquals(0, num, "Expecting 1 + 1 to be 0");
+    public void addingMaxIntPlusOne() {
+        int num = calc.add(Integer.MAX_VALUE, 1);
+        assertNotEquals(Integer.MAX_VALUE + 1L, num, "Expecting 2147483647 + 1 to be 2147483648");
     }
 
     @Test
-    public void addingMinusOnePlusMinusOne() {
-        int num = calc.add(-1, -1);
-        assertEquals(-2, num, "Expecting 1 + 1 to be 0");
+    public void subtractZeroMinusZero() {
+        int num = calc.subtract(0, 0);
+        assertEquals(0, num, "Expecting 0 - 0 to be 0");
+    }
+
+    @Test
+    public void subtractOneMinusOne() {
+        int num = calc.subtract(1, 1);
+        assertEquals(0, num, "Expecting 1 - 1 to be 0");
+    }
+
+    @Test
+    public void multiplyZeroTimesZero() {
+        int num = calc.multiply(0, 0);
+        assertEquals(0, num, "Expecting 0 * 0 to be 0");
+    }
+
+    @Test
+    public void multiplyOneTimesOne() {
+        int num = calc.multiply(1, 1);
+        assertEquals(1, num, "Expecting 1 * 1 to be 1");
+    }
+
+    @Test
+    public void divideZeroByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(0, 0), "Expecting dividing by zero exception.");
+    }
+
+    @Test
+    public void divideOneByOne() {
+        int num = calc.divide(1, 1);
+        assertEquals(1, num, "Expecting 1 * 1 to be 1");
+    }
+
+    @Test
+    public void annuityExample() {
+        String answer = calc.calcAnnuity("22000", 7, ".06", 1);
+        assertEquals("$184,664.43", answer);
+    }
+
+    @Test
+    public void annuityPractice2() {
+        String answer = calc.calcAnnuity("1200", 10, ".08", 4);
+        assertEquals("$72,482.38", answer);
     }
 }
